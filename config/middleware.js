9a8abf5bcdefcbc857467b55397aaf1eb23e2319
Peter Akaliro <pin4eva@hotmail.com>
2022-04-24 17:36:23 +0100

    init

---
+++ config/middleware.js
@@ -0,0 +1,33 @@
+const prodOrigins = [
+    'http://217.21.78.84',
+    'http://server.bdmis.org',
+    'https://server.bdmis.org',
+    'https://cms.bdmis.org',
+    'http://cms.bdmis.org',
+    'https://bdmis.org',
+    'https://www.bdmis.org',
+    'https://portal.bdmis.org',
+    'https://bdmis-h5qqk6wbm-pin4eva.vercel.app',
+    'https://bdmis.vercel.app',
+];
+const devOrigins = [
+    'http://217.21.78.84',
+    'https://cms.bdmis.org',
+    'http://cms.bdmis.org',
+    'https://bdmis.org',
+    'https://www.bdmis.org',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:8000',
+    'http://localhost:8001',
+    'http://localhost',
+];
+
+module.exports = ({ env }) => ({
+    settings: {
+        cors: {
+            enable: true,
+            origin: process.env.NODE_ENV === 'production' ? prodOrigins : devOrigins,
+        },
+    },
+});
